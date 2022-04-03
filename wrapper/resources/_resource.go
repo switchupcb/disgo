2b60f3129d3172ba4743001ea6b614093a32df51
switchupcb <contact@switchupcb.com>
2022-04-03 18:02:25 -0500

    add flag alias to maintain correctness

--- wrapper/resources/_resource.go
+++ wrapper/resources/_resource.go
@@ -1,2 +1,11 @@
 // Package resources represents Discord API Objects.
 package resources
+
+// Flag represents an (unused) alias for a Discord API Flag ranging from 0 - 255.
+type Flag uint8
+
+// BitFlag represents an alias for a Discord API Bitwise Flag denoted by 1 << x.
+type BitFlag uint
+
+// CodeFlag represents an alias for a Discord API code ranging from 0 - 65535.
+type CodeFlag uint16
