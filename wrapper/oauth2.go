b41edbb65faaeae47b5bfc927322729a83da4191
switchupcb <contact@switchupcb.com>
2022-05-20 22:56:15 -0500

    add oauth2 scopes
    Isolate Scopes
    ```
    ^(\w*\.*\w*\.*\w*\.*\w*)\s.*
    
    ```
    
    ```
    (.*) -> FlagOAuth2Scope$1 = "$1"
    ```
    
    manual

---
+++ wrapper/oauth2.go
@@ -0,0 +1,33 @@
+package wrapper
+
+// OAuth2 Scopes
+// https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+const (
+	FlagOAuth2ScopeActivitiesRead                        = "activities.read"
+	FlagOAuth2ScopeActivitiesWrite                       = "activities.write"
+	FlagOAuth2ScopeApplicationsBuildsRead                = "applications.builds.read"
+	FlagOAuth2ScopeApplicationsBuildsUpload              = "applications.builds.upload"
+	FlagOAuth2ScopeApplicationsCommands                  = "applications.commands"
+	FlagOAuth2ScopeApplicationsCommandsUpdate            = "applications.commands.update"
+	FlagOAuth2ScopeApplicationsCommandsPermissionsUpdate = "applications.commands.permissions.update"
+	FlagOAuth2ScopeApplicationsEntitlements              = "applications.entitlements"
+	FlagOAuth2ScopeApplicationsStoreUpdate               = "applications.store.update"
+	FlagOAuth2ScopeBot                                   = "bot"
+	FlagOAuth2ScopeConnections                           = "connections"
+	FlagOAuth2ScopeDM_channelsRead                       = "dm_channels.read"
+	FlagOAuth2ScopeEmail                                 = "email"
+	FlagOAuth2ScopeGDMJoin                               = "gdm.join"
+	FlagOAuth2ScopeGuilds                                = "guilds"
+	FlagOAuth2ScopeGuildsJoin                            = "guilds.join"
+	FlagOAuth2ScopeGuildsMembersRead                     = "guilds.members.read"
+	FlagOAuth2ScopeIdentify                              = "identify"
+	FlagOAuth2ScopeMessagesRead                          = "messages.read"
+	FlagOAuth2ScopeRelationshipsRead                     = "relationships.read"
+	FlagOAuth2ScopeRPC                                   = "rpc"
+	FlagOAuth2ScopeRPCActivitiesWrite                    = "rpc.activities.write"
+	FlagOAuth2ScopeRPCNotificationsRead                  = "rpc.notifications.read"
+	FlagOAuth2ScopeRPCVoiceRead                          = "rpc.voice.read"
+	FlagOAuth2ScopeRPCVoiceWrite                         = "rpc.voice.write"
+	FlagOAuth2ScopeVoice                                 = "voice"
+	FlagOAuth2ScopeWebhookIncoming                       = "webhook.incoming"
+)
