cd0d5ae6d5a776407d3b7a2cd44470eb15eab5ec
switchupcb <contact@switchupcb.com>
2022-05-21 00:19:15 -0500

    add Authorization Code Grant outline

--- wrapper/client.go
+++ wrapper/client.go
@@ -4,6 +4,30 @@ import "github.com/valyala/fasthttp"
 
 // Client represents a Discord Application.
 type Client struct {
+	Authorization *Authorization
+
 	ApplicationID string
 	client        *fasthttp.Client
+}
+
+// Authorization represents authorization parameters required to authorize a client.
+type Authorization struct {
+	// ClientID represents the application's client_id.
+	ClientID string
+
+	// ClientSecret represents the application's client_secret.
+	ClientSecret string
+
+	// Scopes represents a list of OAuth2 scopes.
+	Scopes []string
+
+	// RedirectURI represents the registered url-encoded URL of the application.
+	RedirectURI string
+
+	// state represents the state parameter used to prevent CSRF and Clickjacking.
+	// https://discord.com/developers/docs/topics/oauth2#state-and-security
+	State string
+
+	// prompt controls how the authorization flow handles existing authorizations.
+	Prompt string
 }
--- wrapper/oauth2.go
+++ wrapper/oauth2.go
@@ -1,5 +1,11 @@
 package wrapper
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
 // OAuth2 Scopes
 // https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
 const (
@@ -31,3 +37,113 @@ const (
 	FlagOAuth2ScopeVoice                                 = "voice"
 	FlagOAuth2ScopeWebhookIncoming                       = "webhook.incoming"
 )
+
+// OAuth2 URLs
+// https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-urls
+// TODO: swap to dasgo endpoint function.
+const (
+	EndpointBaseAuthorizationURL   = "https://discord.com/api/oauth2/authorize"
+	EndpointBaseTokenURL           = "https://discord.com/api/oauth2/token"
+	EndpointBaseTokenRevocationURL = "https://discord.com/api/oauth2/token/revoke"
+)
+
+// ContentTypeURL represents an HTTP header that indicates a encoded URL.
+var ContentTypeURL = []byte("application/x-www-form-urlencoded")
+
+// AuthorizationCodeGrant performs an OAuth2 authorization code grant.
+//https://discord.com/developers/docs/topics/oauth2#authorization-code-grant
+func AuthorizationCodeGrant(bot *Client) string {
+	// retrieve an access code.
+	authorizationURL := AuthorizationURL(bot)
+	// get request to authorizationURL for code.
+
+	// exchange the access code for a user's access token.
+	/*
+		data := map[string]string{
+			"client_id":     bot.Authorization.ClientID,
+			"client_secret": bot.Authorization.ClientSecret,
+			"grant_type":    "authorization_code",
+			"code":          code,
+			"redirect_uri":  bot.Authorization.RedirectURI,
+		}
+		header := ContentTypeURL
+	*/
+
+	// post request to access token url
+	// marshal response to AccessTokenResponse
+	// set bot AccessToken information.
+
+	return authorizationURL
+}
+
+// AuthorizationURL generates an authorization URL from a given client
+func AuthorizationURL(bot *Client) string {
+	params := make([]string, 0, 5)
+
+	// client_id is the application client id.
+	params = append(params, "client_id="+bot.Authorization.ClientID)
+
+	// scope is a list of OAuth2 scopes separated by url encoded spaces (%20).
+	if len(bot.Authorization.Scopes) > 0 {
+		var scope strings.Builder
+		scope.WriteString("scope=")
+
+		for i, s := range bot.Authorization.Scopes {
+			if i > 0 {
+				scope.WriteString("%20")
+			}
+
+			scope.WriteString(s)
+		}
+		params = append(params, scope.String())
+	}
+
+	// redirect_uri is the URL registered while creating the application.
+	if bot.Authorization.RedirectURI != "" {
+		params = append(params, "redirect_uri="+bot.Authorization.RedirectURI)
+	}
+
+	// state is the unique string mentioned in State and Security.
+	if bot.Authorization.State != "" {
+		params = append(params, "state="+bot.Authorization.State)
+	}
+
+	// prompt controls how the authorization flow handles existing authorizations.
+	if bot.Authorization.Prompt != "" {
+		params = append(params, "prompt="+bot.Authorization.Prompt)
+	}
+
+	return EndpointBaseAuthorizationURL + "?response_type=code&" + strings.Join(params, "&")
+}
+
+// Access Token Response
+// https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-access-token-response
+type AccessTokenResponse struct {
+	AccessToken  string        `json:"access_token,omitempty"`
+	TokenType    string        `json:"token_type,omitempty"`
+	ExpiresIn    time.Duration `json:"expires_in,omitempty"`
+	RefreshToken string        `json:"refresh_token,omitempty"`
+	Scope        string        `json:"scope,omitempty"`
+}
+
+// RefreshAccessTokenExchange refreshes the given client's access token.
+func RefreshAccessTokenExchange(bot *Client) error {
+	if bot.AccessToken == nil {
+		return fmt.Errorf("cannot refresh access token without access token information.")
+	}
+
+	/*
+		// post request to access token url
+		data = {
+			"client_id": bot.Authorization.ClientID,
+			"client_secret": bot.Authorization.ClientSecret,
+			"grant_type": "refresh_token",
+			"refresh_token": bot.AccessToken.RefreshToken
+		  }
+		  headers = {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		  }
+	*/
+
+	return nil
+}
