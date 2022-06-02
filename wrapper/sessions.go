548f8b3fda25fc47cebb0bb569c97eeef6662b9d
t-rog <tommmyr9@gmail.com>
2022-06-01 22:11:14 -0500

    add Reconn and Terminate

--- wrapper/sessions.go
+++ wrapper/sessions.go
@@ -15,9 +15,10 @@ const (
 	module                    = "github.com/switchupcb/disgo"
 	maxIdentifyLargeThreshold = 250
 	ErrEventUnmarshal         = "an error occurred while unmarshalling a %v Event:\n%w"
+	statusCode                = 1000
 )
 
-// Session represents a session.
+// Session represents a Discord Gateway WebSocket Session.
 type Session struct {
 	// Token represents the Bot token the session used to connect.
 	Token string
@@ -29,11 +30,17 @@ type Session struct {
 	//
 	// https://discord.com/developers/docs/topics/gateway#heartbeat
 	Seq int
+
+	// Endpoint represents the endpoint that will be connected to.
+	Endpoint string
+
+	// Connected represented whether or not the session is connected to Discord Gateway.
+	Connected bool
 }
 
-// Connect creates open connection to Discord Gateway.
+// Connect creates an open connection to Discord Gateway.
 func (bot *Client) Connect(s *Session) error {
-	// get the Gateway URL.
+	// requesting a valid Gateway URL endpoint from the API.
 	gateway := GetGateway{}
 	response, err := gateway.Send(bot)
 	if err != nil {
@@ -46,16 +53,16 @@ func (bot *Client) Connect(s *Session) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	// connect to Discord Gateway Websocket.
+	// connect to the Discord Gateway Websocket.
 	conn, _, err := websocket.Dial(ctx, endpoint, nil)
 	if err != nil {
 		return fmt.Errorf("an error occurred performing WebSocket handshake\n%w", err)
 	}
 
 	defer conn.Close(websocket.StatusInternalError, "StatusInternalError")
+	s.Connected = true
 
-	// unmarshall json file bytes into hello event object
-	// handle the Hello event
+	// handle the incoming Hello event (JSON) upon connecting to the Gateway.
 	var hello Hello
 	err = wsjson.Read(ctx, conn, &hello)
 	if err != nil {
@@ -66,7 +73,7 @@ func (bot *Client) Connect(s *Session) error {
 	// go send heartbeat
 	// TODO: deal with heartbeat when listening for events (2nd paragraph)
 
-	// deal with opcode 2
+	// send an Identify event to the Discord Gateway.
 	event := Identify{
 		Token: bot.Authentication.Token,
 		Properties: IdentifyConnectionProperties{
@@ -83,7 +90,7 @@ func (bot *Client) Connect(s *Session) error {
 
 	wsjson.Write(ctx, conn, event)
 
-	// Reading in Ready event.
+	// handle the incoming Ready event upon identification with the socket.
 	var ready Ready
 	err = wsjson.Read(ctx, conn, &ready)
 	if err != nil {
@@ -94,13 +101,53 @@ func (bot *Client) Connect(s *Session) error {
 	return nil
 }
 
-func (s *Session) Reconn(s *Session) {
+// Reconn restablishes the connection to the Discord Gateway.
+func (s *Session) Reconn() error {
 
+	if s.Connected {
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	// connect to Discord Gateway Websocket.
+	conn, _, err := websocket.Dial(ctx, s.Endpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	// send a Resume event to the Discord Gateway.
 	event := Resume{
 		Token:     s.Token,
 		SessionID: s.ID,
 		Seq:       uint32(s.Seq),
 	}
+
+	err = wsjson.Write(ctx, conn, event)
+	if err != nil {
+		return fmt.Errorf(ErrEventUnmarshal, "Resume", err)
+	}
+
+	// read in the Resumed event to access its SessionID
+	// TODO: handle all of the events in order
+	// TODO: handle opcode 9
+	var resumed Resumed
+	err = wsjson.Read(ctx, conn, &resumed)
+	if err != nil {
+		return fmt.Errorf(ErrEventUnmarshal, "Resumed", err)
+	}
+	if resumed.Op != 7 {
+		return fmt.Errorf(ErrEventUnmarshal, "Ready", err)
+	}
+
+	return nil
 }
 
-// find out how to terminate connection
+// Terminate disconnects from the Discord Gateway by sending a status code 1000.
+func Terminate(c *websocket.Conn) error {
+
+	c.Close(statusCode, "Close TCP Connection")
+
+	return nil
+}
