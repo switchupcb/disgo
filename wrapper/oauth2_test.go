760e8e392f7d0daf87a175b24e4304c117b287de
Josh Dawe <joshdawe00@gmail.com>
2022-06-15 23:25:43 -0500

    add unit test for OAuth2

---
+++ wrapper/oauth2_test.go
@@ -0,0 +1,151 @@
+package wrapper
+
+import (
+	"testing"
+)
+
+// testOAuth2 represents parameters used to test GenerateAuthorizationURL.
+type testOAuth2 struct {
+	name     string
+	bot      *Client
+	response string
+	output   string
+}
+
+// testOAuth2Bot represents parameters used to test GenerateBotAuthorizationURL.
+type testOAuth2Bot struct {
+	name   string
+	p      BotAuthParams
+	output string
+}
+
+// TestGenerateAuthorizationURL tests GenerateAuthorizationURL for a valid authorization URL.
+func TestGenerateAuthorizationURL(t *testing.T) {
+	tests := []testOAuth2{
+		{
+			name: "test1",
+			bot: &Client{
+				Authorization: &Authorization{
+					ClientID:     "988406086973444138",
+					ClientSecret: "",
+					Scopes:       []string{"scope1", "scope2"},
+					RedirectURI:  "https://localhost",
+					State:        "state",
+					Prompt:       "prompt",
+				},
+			},
+			response: "code",
+			output:   EndpointAuthorizationURL() + "?responsetype=code&client_id=988406086973444138&scope=scope1%20scope2&redirect_uri=https%3A%2F%2Flocalhost&state=state&prompt=prompt",
+		},
+		{
+			name: "test2",
+			bot: &Client{
+				Authorization: &Authorization{
+					ClientID:     "983406086973444138",
+					ClientSecret: "",
+					Scopes:       []string{"bot", "applications.commands"},
+					RedirectURI:  "https://localhost",
+					State:        "",
+					Prompt:       "",
+				},
+			},
+			response: "",
+			output:   EndpointAuthorizationURL() + "?client_id=983406086973444138&scope=bot%20applications.commands&redirect_uri=https%3A%2F%2Flocalhost",
+		},
+		{
+			name: "test3",
+			bot: &Client{
+				Authorization: &Authorization{
+					ClientID:     "406086983973444138",
+					ClientSecret: "",
+					Scopes:       []string{},
+					RedirectURI:  "https://localhost",
+					State:        "",
+					Prompt:       "",
+				},
+			},
+			response: "",
+			output:   EndpointAuthorizationURL() + "?client_id=406086983973444138&redirect_uri=https%3A%2F%2Flocalhost",
+		},
+	}
+
+	for _, test := range tests {
+		got := GenerateAuthorizationURL(test.bot, test.response)
+		if got != test.output {
+			t.Errorf("(%v: got %v, wanted %v", test.name, got, test.output)
+		}
+	}
+}
+
+// TestGenerateBotAuthorizationURL tests GenerateBotAuthorizationURL for a valid bot authorization URL.
+func TestGenerateBotAuthorizationURL(t *testing.T) {
+	tests := []testOAuth2Bot{
+		{
+			name: "botTest1",
+			p: BotAuthParams{
+				Bot: &Client{
+					Authorization: &Authorization{
+						ClientID:     "988406086973444138",
+						ClientSecret: "",
+						Scopes:       []string{"scope1", "scope2"},
+						RedirectURI:  "https://github.com",
+						State:        "state",
+						Prompt:       "prompt",
+					},
+				},
+				Permissions:        1,
+				GuildID:            "9862629927296967350",
+				DisableGuildSelect: false,
+				ResponseType:       "",
+			},
+			output: EndpointAuthorizationURL() + "?client_id=988406086973444138&scope=scope1%20scope2&redirect_uri=https%3A%2F%2Fgithub.com&state=state&prompt=prompt&permissions=1&guild_id=9862629927296967350&disable_guild_select=false",
+		},
+		{
+			name: "botTest2",
+			p: BotAuthParams{
+				Bot: &Client{
+					Authorization: &Authorization{
+						ClientID:     "983406086973444138",
+						ClientSecret: "",
+						Scopes:       []string{"bot", "applications.commands"},
+						RedirectURI:  "https://github.com",
+						State:        "",
+						Prompt:       "",
+					},
+				},
+				Permissions:        10,
+				GuildID:            "2909267986263572999",
+				DisableGuildSelect: true,
+				ResponseType:       "code",
+			},
+			output: EndpointAuthorizationURL() + "?responsetype=code&client_id=983406086973444138&scope=bot%20applications.commands&redirect_uri=https%3A%2F%2Fgithub.com&permissions=10&guild_id=2909267986263572999&disable_guild_select=true",
+		},
+		{
+			name: "botTest3",
+			p: BotAuthParams{
+				Bot: &Client{
+					Authorization: &Authorization{
+						ClientID:     "406086983973444138",
+						ClientSecret: "",
+						Scopes:       []string{},
+						RedirectURI:  "https://localhost",
+						State:        "",
+						Prompt:       "",
+					},
+				},
+				Permissions:        10,
+				GuildID:            "2909267986263572999",
+				DisableGuildSelect: true,
+				ResponseType:       "code",
+			},
+			output: EndpointAuthorizationURL() + "?responsetype=code&client_id=406086983973444138&redirect_uri=https%3A%2F%2Flocalhost&permissions=10&guild_id=2909267986263572999&disable_guild_select=true",
+		},
+	}
+
+	for _, test := range tests {
+		got := GenerateBotAuthorizationURL(test.p)
+		if got != test.output {
+			t.Errorf("(%v: got %v, wanted %v", test.name, got, test.output)
+		}
+	}
+}
