013cc4df86c19d2275572b9826c93ff7c7874283
switchupcb <contact@switchupcb.com>
2022-07-19 20:17:34 -0500

    refactor global rate limit test

--- wrapper/ratelimit_test.go
+++ wrapper/ratelimit_test.go
@@ -1,8 +1,12 @@
 package wrapper
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 // TestGlobalRateLimit tests the global rate limit mechanism.
@@ -20,38 +24,41 @@ func TestGlobalRateLimit(t *testing.T) {
 	requests := 151
 
 	// prepare the test tracking variables.
-	errs := make(chan error)
-	responses := make(chan *Application, requests)
+	eg, ctx := errgroup.WithContext(context.Background())
 
 	// send 51 requests concurrently.
 	for i := 1; i <= requests; i++ {
 		select {
-		case err := <-errs:
-			t.Fatalf("%v", err)
+		case <-ctx.Done():
+			t.Fatalf("%v", eg.Wait())
 		default:
 		}
 
-		go func(id int) {
+		id := i
+		eg.Go(func() error {
 			t.Log("Spawned request goroutine", id)
+
 			app, err := request.Send(bot)
 			if err != nil {
-				errs <- err
+				return err
 			}
 
 			t.Log("Request", id, ":", app)
-			responses <- app
-		}(i)
+
+			return nil
+		})
 	}
 
-	// wait until all requests are sent.
-	for {
-		select {
-		case err := <-errs:
-			t.Fatalf("%v", err)
-		default:
-			if len(responses) == cap(responses) {
-				return
-			}
-		}
+	// wait until all requests are sent and responses are received.
+	if err := eg.Wait(); err != nil {
+		t.Fatalf("%v", err)
 	}
+
+	// ensure that the next test starts from a full bucket.
+	time.After(time.Second)
+}
+
+// TestRouteRateLimit tests the per-route rate limit mechanism.
+func TestRouteRateLimit(t *testing.T) {
+
 }
