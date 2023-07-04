517e563e6672ec063254a2b2ad574865b2ef8a5c
switchupcb <81384235+switchupcb@users.noreply.github.com>
2023-07-04 00:42:47 -0500

    lint generator and tests

--- wrapper/socket/socket.go
+++ wrapper/socket/socket.go
@@ -16,7 +16,7 @@ import (
 func Read(ctx context.Context, conn *websocket.Conn, dst any) error {
 	messageType, reader, err := conn.Reader(ctx)
 	if err != nil {
-		return err
+		return err //nolint:wrapcheck
 	}
 
 	// reuse buffers in between calls to avoid allocations.
@@ -28,29 +28,29 @@ func Read(ctx context.Context, conn *websocket.Conn, dst any) error {
 	case websocket.MessageText:
 		// read the message.
 		if _, err := b.ReadFrom(reader); err != nil {
-			return err
+			return err //nolint:wrapcheck
 		}
 
 		// unmarshal the message into dst.
 		if err = json.Unmarshal(b.Bytes(), &dst); err != nil {
-			return fmt.Errorf("socket.Read (websocket.MessageText) to %T: %w\n%s", dst, err, string(b.Bytes()))
+			return fmt.Errorf("socket.Read (websocket.MessageText) to %T: %w\n%s", dst, err, b.String())
 		}
 
 	case websocket.MessageBinary:
 		zlibReader, err := zlib.NewReader(reader)
 		if err != nil {
-			return err
+			return err //nolint:wrapcheck
 		}
 		defer zlibReader.Close()
 
 		// read the message.
 		if _, err := b.ReadFrom(zlibReader); err != nil {
-			return err
+			return err //nolint:wrapcheck
 		}
 
 		// unmarshal the message into dst.
 		if err = json.Unmarshal(b.Bytes(), &dst); err != nil {
-			return fmt.Errorf("socket.Read (websocket.MessageBinary) to %T: %w\n%s", dst, err, string(b.Bytes()))
+			return fmt.Errorf("socket.Read (websocket.MessageBinary) to %T: %w\n%s", dst, err, b.String())
 		}
 
 	default:
@@ -64,12 +64,12 @@ func Read(ctx context.Context, conn *websocket.Conn, dst any) error {
 func Write(ctx context.Context, conn *websocket.Conn, m websocket.MessageType, dst any) error {
 	writer, err := conn.Writer(ctx, m)
 	if err != nil {
-		return err
+		return err //nolint:wrapcheck
 	}
 
 	if err := json.NewEncoder(writer).Encode(dst); err != nil {
-		return err
+		return err //nolint:wrapcheck
 	}
 
-	return writer.Close()
+	return writer.Close() //nolint:wrapcheck
 }
